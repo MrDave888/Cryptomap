897566ed0194cbe2902ec6b6e06a2403f09c8910
Dave Jones <Mr.David_Jones@hotmail.co.uk>
2018-02-08 10:10:06 +0000

    Cleaned up the look of the application

--- src/imports/components/Header.js
+++ src/imports/components/Header.js
@@ -5,6 +5,21 @@ export default class Header extends Component {
     return (
       <div className="header-container">
         <h1> CRYPTOMAP </h1>
+        <div className="header-actions">
+          <select name="currency">
+            <option value="BTC">Bitcoin</option>
+            <option value="ETH">Ethereum</option>
+            <option value="BCH">Bitcoin Cash</option>
+            <option value="XPR">Ripple</option>
+          </select>
+          <select name="type">
+            <option value="ATM">ATM</option>
+            <option value="Bookstore">Bookstore</option>
+            <option value="Restraunt">Restraunt</option>
+            <option value="Supermarket">Supermarket</option>
+          </select>
+          <input type="text" placeholder="Search" ref="search"/>
+        </div>
       </div>
     );
   }
